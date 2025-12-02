66bd313d281b4403ec425bb128a477eab9e39f87
sinbc2003 <sinbc2003@gmail.com>
2025-12-02 14:36:54 +0900

    방만들고 게임진행 수정11

--- backend/app/schemas/tournament.py
+++ backend/app/schemas/tournament.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from ..enums import TournamentStatus, RoundType
 
